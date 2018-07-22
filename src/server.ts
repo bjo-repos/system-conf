dd99743c26ffddbbdfd9730491503144acdade09
Benjamin Joly <joly.benjamin@gmail.com>
2018-07-22 09:46:38 +0200

    Add nodemon for server uto restart on (re)build

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,23 @@
-// const express = require('express');
-
 import express from 'express';
+import { Router, Request, Response, Application } from 'express';
 
-const app = express();
-const router = express.Router();
+const app: Application = express();
+const router = Router();
 
-router.get('/', function (req, res) {
+const port: number = Number(process.env.PORT) || 3000;
+
+router.get('/', function (req: Request, res: Response) {
   res.json({message: 'Welcome to System Conf API'});
 })
 
+router.get('/users', function (req: Request, res: Response) {
+  res.json({message: 'Users API'});
+})
+
 // all routes will be prefixed with /api
-// app.use('/api', router);
 app.use('/api', router);
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000');
+// Start server here, linteining on port 3000
+app.listen(port, function () {
+  console.log(`Listening @ at http://localhost:${port}/api `);
 })
